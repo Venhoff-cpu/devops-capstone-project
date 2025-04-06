51dea8f06721b9585aebad2a4d3ddd4c0d62e1a9
Krzysztof Pacuszka <krzysztof.pacuszka@gmail.com>
2025-04-06 13:20:36 +0200

    Update test_routes.py - list tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -128,13 +128,7 @@ class TestAccountService(TestCase):
     ######################################################################
     def test_get_account(self):
         """It should Read an Account that is found"""
-        account = AccountFactory()
-        response = self.client.post(
-            BASE_URL, 
-            json=account.serialize(), 
-            content_type="application/json"
-        )
-        new_account = response.get_json()
+        self._create_accounts(1)
         resp = self.client.get(f"{BASE_URL}/{new_account['id']}")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
     
@@ -142,3 +136,23 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    ######################################################################
+    # READ AN ACCOUNT
+    ######################################################################
+    def test_get_account_list(self):
+        """It should Get a list of Accounts"""
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
+
+    def test_get_account_list_empty(self):
+        """It should return empty list"""
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 0)
+        self.assertIsInstance(data, list)
+  
